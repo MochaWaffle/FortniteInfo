9b986a42d8d2b340d9cc39fad5a7f282aee21260
MochaWaffle <94003964+MochaWaffle@users.noreply.github.com>
2023-12-28 15:51:35 -0500

    Fetched news information

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,5 +1,33 @@
+import {useEffect, useState} from 'react';
+
 export default function News() {
+    function getNews() {
+        fetch('https://fortnite-api.com/v2/news')
+        .then((response) => response.json())
+        .then((data) => {
+            setNews(data),
+            setNewsTitle(data.data.br.motds[0].title),
+            setNewsImage(data.data.br.motds[0].image),
+            setNewsText(data.data.br.motds[0].body)
+        });
+    }
+    const[news, setNews] = useState([]);
+    const[newsTitle, setNewsTitle] = useState('');
+    const[newsImage, setNewsImage] = useState('');
+    const[newsText, setNewsText] = useState('');
+    useEffect(() => {getNews()},[]);
+    console.log(news);
+    console.log("News Title:" + newsTitle);
+    console.log("News Image Link: " + newsImage);
     return (
-        <h1> News</h1>
+        <>
+            <h1> {newsTitle} </h1>
+            <div className = "mapContainer">
+                <a href={newsImage} target="_blank"><img src={newsImage} className="mapImage" alt="Current Fortnite News Image"></img></a>
+            </div>
+            <p>"{newsText}"</p>
+        </>
     )
+
+    
 }
