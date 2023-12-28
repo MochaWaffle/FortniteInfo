c9ef8446d9ed4e0b3f3107f365051ab43a2a5ad1
MochaWaffle <94003964+MochaWaffle@users.noreply.github.com>
2023-12-28 17:40:18 -0500

    Fetched creator code and added text
    The website displays whether or not a creator code is valid.

--- src/pages/CreaterCode.jsx
+++ src/pages/CreaterCode.jsx
@@ -1,8 +1,44 @@
 
+import {useState} from 'react';
+
 export default function CreaterCode() {
+    const[code, setCode] = useState("");
+    const[isValid, setIsValid] = useState("");
+
+    function isCodeValid() {
+        if (code !== "") {
+            fetch('https://fortnite-api.com/v2/creatorcode?name=' + code)
+            .then((response) => response.json())
+            .then((data) => {    
+                if (data.status === 404) {
+                    setIsValid("Invalid Code");
+                } else if (data.status === 200) {
+                    if (data.data.status === "ACTIVE") {
+                        setIsValid("Valid Code");
+                    } else {
+                        setIsValid("Invalid Code");
+                    }
+                }
+            });
+        } else {
+            setIsValid("");
+        }
+    }
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        isCodeValid();
+    }
     return (
         <>
             <h1>Creater Code Validator</h1>
+
+            <input type="text" onChange = {(e) => setCode(e.target.value)} placeholder="Enter Code" />
+            <button onClick= {handleSubmit}>Submit</button>
+
+            {isValid !== '' &&
+                <p>{isValid}</p>
+            }
         </>
     )
 }
