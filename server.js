e5897b7086e168d88f6d96c5eaf716065f5338cd
MochaWaffle <94003964+MochaWaffle@users.noreply.github.com>
2023-12-28 12:13:39 -0500

    Created server javascript

---
+++ server.js
@@ -0,0 +1,31 @@
+// defining the server port
+const port = 5000
+
+// initializing installed dependencies
+const express = require('express')
+require('dotenv').config()
+const axios = require('axios')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+
+// listening for port 5000
+app.listen(5000, ()=> console.log(`Server is running on ${port}` ))
+
+// API request
+app.get('/', (req,res)=>{    
+    const options = {
+        method: 'GET',
+        url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions',
+        headers: {
+            'X-RapidAPI-Key':process.env.REACT_APP_API_KEY,
+            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+        }
+   };
+   
+    axios.request(options).then(function (response) {
+        res.json(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+})
