350a8c531fb65fe98eb77bd3987c3e86c50d6fab
MochaWaffle <94003964+MochaWaffle@users.noreply.github.com>
2023-12-28 19:47:34 -0500

    Updated server and stats

--- server.cjs
+++ server.cjs
@@ -1,30 +1,62 @@
-// defining the server port
-const port = 5000
+// // defining the server port
+// const port = 5000
 
-// initializing installed dependencies
+// // initializing installed dependencies
+// const express = require('express')
+// require('dotenv').config()
+// const axios = require('axios')
+// const app = express()
+// const cors = require('cors')
+// app.use(cors())
+
+// // listening for port 5000
+// app.listen(port, ()=> console.log(`Server is running on ${port}` ))
+
+// // API request
+// app.get('/', (req,res)=>{    
+//     const playerName = req.query.name;
+//     const options = {
+//         method: 'GET',
+//         url: 'https://fortnite-api.com/v2/stats/br/v2?name=' + playerName,
+//         headers: {
+//             'Authorization':process.env.VITE_API_KEY,
+//         }
+//    };
+   
+//     axios.request(options).then(function (response) {
+//         res.json(response.data);
+//     }).catch(function (error) {
+//         console.error(error);
+//     });
+// })
+
+const PORT = 8000
 const express = require('express')
-require('dotenv').config()
-const axios = require('axios')
-const app = express()
 const cors = require('cors')
+const axios = require('axios')
+require('dotenv').config()
+
+const app = express()
+
 app.use(cors())
 
-// listening for port 5000
-app.listen(5000, ()=> console.log(`Server is running on ${port}` ))
-
-// API request
-app.get('/', (req,res)=>{    
+app.get('/', (req,res) => {
+    //const playerName = req.query.name;
     const options = {
         method: 'GET',
-        url: 'https://fortnite-api.com/v2/stats/br/v2',
+        url: 'https://fortnite-api.com/v2/stats/br/v2?name=MochaWall864836',
         headers: {
-            'Authorization':import.meta.env.VITE_API_KEY,
+            'Authorization': process.env.VITE_API_KEY
         }
-   };
-   
-    axios.request(options).then(function (response) {
-        res.json(response.data);
-    }).catch(function (error) {
+    }
+
+    axios.request(options).then((response) => {
+        res.json(response.data)
+
+    }).catch((error) => {
         console.error(error);
-    });
+        res.status(500).json({error: 'Failed to fetch player data.'})
+    })
 })
+
+app.listen(8000, () => console.log(`Server is running on port ${PORT}`))
--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,7 +1,62 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+
 export default function Stats() {
+    const[platform, setPlatform] = useState("PC");
+    const[playerName, setPlayerName] = useState("");
+    const[playerData, setPlayerData] = useState([]);
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            url: 'http://localhost:8000'
+        }
+
+        axios.request(options).then((response) => {
+            console.log(response.data)
+            setPlayerData(response.data)
+        }).catch((error) => {
+            console.error(error)
+        })
+    }, [])
+
+    console.log(playerData);
+
+    const handleChange = (event) => {
+        setPlatform(event.target.value)
+    }
+
+    function getPlayerData() {
+        fetch('https://fortnite-api.com/v2/stats/br/v2?name=' + playerName)
+            .then((response) => response.json())
+            .then((data) => {    
+                if (data.status === 404) {
+                    setIsValid("Invalid");
+                } else if (data.status === 200) {
+                    if (data.data.status === "ACTIVE") {
+                        setIsValid("Valid");
+                    } else {
+                        setIsValid("Invalid");
+                    }
+                }
+            });
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        getPlayerData();
+    }
     return (
         <>
             <h1>Stats</h1>
+            <form>
+                <input type="text" onChange = {(e) => setPlayerName(e.target.value)} placeholder="Enter Player Name" />
+                <select value={platform} onChange={handleChange}>
+                    <option value="PC">PC</option>
+                    <option value="Xbox">Xbox</option>
+                    <option value="Playstation">Playstation</option>
+                </select>
+                <button onClick = {handleSubmit}>Submit</button>
+            </form>
         </>
     )
 }
