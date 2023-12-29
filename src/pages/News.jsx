06d8a68ece078c42e821fea27f17b78c6b033150
MochaWaffle <94003964+MochaWaffle@users.noreply.github.com>
2023-12-29 18:33:50 -0500

    Updated map and news alt tag

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -38,7 +38,7 @@ export default function News() {
                     <p><i>{newsText}</i></p>
                 </div>
                 
-                <a href={newsImage} target="_blank"><img src={newsImage} alt="Image could not load. Image was: Current Fortnite News Image"></img></a>
+                <a href={newsImage} target="_blank"><img src={newsImage} alt="Image could not load. Image was: Current Fortnite News Image."></img></a>
             </div>
             
         </>
