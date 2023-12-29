1562ed441e58fee1a67cc3f4fee66847f24634ab
MochaWaffle <94003964+MochaWaffle@users.noreply.github.com>
2023-12-29 12:13:39 -0500

    Updated news style

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,30 +1,44 @@
 import {useEffect, useState} from 'react';
 
 export default function News() {
-    function getNews() {
-        fetch('https://fortnite-api.com/v2/news')
-        .then((response) => response.json())
-        .then((data) => {
-            setNews(data),
-            setNewsTitle(data.data.br.motds[0].title),
-            setNewsImage(data.data.br.motds[0].image),
-            setNewsText(data.data.br.motds[0].body)
-        });
-    }
-    const[news, setNews] = useState([]);
     const[newsTitle, setNewsTitle] = useState('');
     const[newsImage, setNewsImage] = useState('');
     const[newsText, setNewsText] = useState('');
-    useEffect(() => {getNews()},[]);
-    console.log(news);
-    console.log("News Title:" + newsTitle);
-    console.log("News Image Link: " + newsImage);
+
+    useEffect(() => {
+        async function getNews() {
+            try {
+                const response = await fetch('https://fortnite-api.com/v2/news');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+                const data = await response.json();
+
+                if (data && data.data && data.data.br && data.data.br.motds && data.data.br.motds.length > 0) {
+                    setNewsTitle(data.data.br.motds[0].title || 'Currently no news title.');
+                    setNewsImage(data.data.br.motds[0].image || '');
+                    setNewsText(data.data.br.motds[0].body || 'Currently no news text.');
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setNewsTitle("Error fetching news title data");
+                setNewsText("Error fetching news text data");
+            }
+        }
+
+        getNews();
+    }, []);
+  
     return (
         <>
             <h1> {newsTitle} </h1>
-            <p>"{newsText}"</p>
+            
             <div className = "mapContainer">
-                <a href={newsImage} target="_blank"><img src={newsImage} className="mapImage" alt="Current Fortnite News Image"></img></a>
+                <div className="newsText">
+                    <p>"{newsText}"</p>
+                </div>
+                
+                <a href={newsImage} target="_blank"><img src={newsImage} className="mapImage" alt="Image could not load. Image was: Current Fortnite News Image"></img></a>
             </div>
             
         </>
