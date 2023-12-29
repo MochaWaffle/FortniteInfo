74f516c2bb3b4e67b56336303f05b7214e490189
MochaWaffle <94003964+MochaWaffle@users.noreply.github.com>
2023-12-29 15:36:56 -0500

    Updated Navbar responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { Route, Routes } from "react-router-dom"
 
 function App() {
   console.log('Width: ' + window.innerWidth)
+  console.log("Height: " + window.innerHeight)
   return (
     <>
       <Navbar />
