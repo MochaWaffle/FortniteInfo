61b4d06c75b22932f36f727a4710758ed52d593d
MochaWaffle <94003964+MochaWaffle@users.noreply.github.com>
2023-12-29 19:43:03 -0500

    Changed background and home design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,13 @@ function Home() {
         </div> */}
             
             
-        <img className="backgroundImage" src={Fortnite_Wallpaper}/>
-
+        {/* <img className="backgroundImage" src={Fortnite_Wallpaper}/> */}
+        <div className="parentHomeContainer">
+            <div className="homeContainer">
+                <h1 className="homeTitle">Welcome!</h1>
+                <p className="homeBodyText">This website uses the API: <a href="https://fortnite-api.com/">https://fortnite-api.com/</a></p>
+            </div>
+        </div>
         </>
     )
 }
