9c1c6a90815e0d6d7dd33a6ad4a8842537c2f6ff
MochaWaffle <94003964+MochaWaffle@users.noreply.github.com>
2023-12-29 19:44:52 -0500

    Cleaned Home.jsx and App.css code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,6 @@ import Fortnite_Wallpaper from '../Images/Fortnite_Wallpaper.jpg'
 function Home() {
     return (
         <>
-        {/* <div className="backgroundContainer">
-            <h1 className="title">Welcome</h1>
-        </div> */}
-            
-            
-        {/* <img className="backgroundImage" src={Fortnite_Wallpaper}/> */}
         <div className="parentHomeContainer">
             <div className="homeContainer">
                 <h1 className="homeTitle">Welcome!</h1>
