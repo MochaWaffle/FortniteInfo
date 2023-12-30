7b6cb1a20862e73fb6e0510603ea2b292f6bd10e
MochaWaffle <94003964+MochaWaffle@users.noreply.github.com>
2023-12-30 12:35:24 -0500

    Updated navbar design.
    Full responsive design.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,40 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import 'boxicons';
 
 export default function Navbar() {
   return (
-    <nav className="nav">
-      <Link to="/" className="site-title">
-        FortniteInfo
-      </Link>
-      <ul>
-        <CustomLink to="/map" className="linkStyle">Map</CustomLink>
-        <CustomLink to="/news" className="linkStyle">News</CustomLink>
-        <CustomLink to="/shop" className="linkStyle">Shop</CustomLink>
-        <CustomLink to="/creatorcode" className="linkStyle">Creator Code</CustomLink>
-      </ul>
-    </nav>
+    // <nav className="nav">
+    //   <Link to="/" className="site-title">
+    //     FortniteInfo
+    //   </Link>
+    //   <ul>
+    //     <CustomLink to="/map" className="linkStyle">Map</CustomLink>
+    //     <CustomLink to="/news" className="linkStyle">News</CustomLink>
+    //     <CustomLink to="/shop" className="linkStyle">Shop</CustomLink>
+    //     <CustomLink to="/creatorcode" className="linkStyle">Creator Code</CustomLink>
+    //   </ul>
+    // </nav>
+    <>
+      <header className="header">
+        <Link to="/" className="logo">FortniteInfo</Link>
+
+        <input type="checkbox" id="check" />
+        <label for="check" className="icons" >
+          <box-icon name='menu' id="menu-icon" color="white" size="2.8rem"></box-icon>
+          <box-icon name='x' id="close-icon" color="white" size="2.8rem"></box-icon>
+        </label>
+        <nav className="navbar">
+          {/* <CustomLink to="/map">Map</CustomLink>
+          <CustomLink to="/news">News</CustomLink>
+          <CustomLink to="/shop">Shop</CustomLink>
+          <CustomLink to="/creatorcode">Creater Code</CustomLink> */}
+          <Link to="/map" style={{'--i': 0}}>Map </Link>
+          <Link to="/news" style={{'--i': 1}}>News </Link>
+          <Link to="/shop" style={{'--i': 2}}>Shop </Link>
+          <Link to="/creatorcode" style={{'--i': 3}}>Creator Code</Link>
+        </nav>
+      </header>
+    </>
   )
 }
 
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,33 @@
+import {useEffect, useState} from 'react'
+
 export default function Shop() {
+    const[shopData, setShopData] = useState([])
+
+    useEffect(() => {
+        async function getShopData() {
+            try {
+                const response = await fetch('https://fortnite-api.com/v2/shop/br');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+                const data = await response.json();
+
+                setShopData(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setShopData([]);
+            }
+        }
+
+        getShopData();
+    }, []);
+    
+       
     return (
         <>
             <h1>Shop (coming soon...)</h1>
+            {shopData.length == 0 && <p>Could not retrieve shop data.</p>}
+
         </>
     )
 }
