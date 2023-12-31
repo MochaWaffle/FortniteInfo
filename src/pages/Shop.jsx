58ca02fb492186e97225a7eb79ed481d55285fd0
MochaWaffle <vsaini23@medford.k12.ma.us>
2023-12-31 16:43:30 -0500

    Added info to cards.

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,6 +3,8 @@ import {useEffect, useState} from 'react'
 export default function Shop() {
     const[shopData, setShopData] = useState([])
     const[shopEntries, setShopEntries] = useState([])
+    const[shopBundle, setShopBundle] = useState([])
+    const[shopItems, setShopItems] = useState([])
 
     useEffect(() => {
         async function getShopData() {
@@ -24,56 +26,77 @@ export default function Shop() {
         getShopData();
     }, []);
     
-    console.log(shopData);
+    //console.log(shopData);
+    console.log(shopEntries);
     return (
         <>
         <div className="containerResize">
             <h1 className="shopTitle">Shop</h1>
             {shopData.length == 0 && <p>Could not retrieve shop data.</p>}
-            
+
             <section className="cardContainer">
-                <div className="card">
-                    <div className="card-image"></div>
-                    <h2 className="cardTextColor">Title</h2>
-                    <p className="cardTextColor">Text</p>
-                    <a href="">READ MORE</a>
-                </div>
+                {shopEntries.map((entries, index) =>
+                    
+                    {if (entries.bundle !== null) {
+                        return (
+                            <>
+                                {/* <div key={index}>
+                                    <br />
+                                    <p>Bundle {index}: {entries.bundle.name}</p>
+                                    <p>Image: {entries.bundle.image} </p>
+                                    <p> Price: {entries.finalPrice} ({entries.regularPrice - entries.finalPrice} off!)</p>
+                                </div> */}
+
+                                <div className="card">
+                                    <div className="card-image" style={{ backgroundImage: `url(${entries.bundle.image})`}}></div>
+                                    <h2 className="cardTextColor">{entries.bundle.name}</h2>
+                                    <p className="cardTextColor">{entries.bundle.info}</p>
+
+                                    {entries.regularPrice - entries.finalPrice > 0 && 
+                                        <a href="">{entries.finalPrice} ({entries.regularPrice - entries.finalPrice} off!)</a>
+                                    }
+                                    
+                                    {entries.regularPrice - entries.finalPrice <= 0 && 
+                                        <a href="">{entries.finalPrice}</a>
+                                    }
+                                </div>
+                            </>
+                        )
+                    } else {
+                        return (
+                            <>
+                                {entries.items.map((item) =>
+                                    <>
+                                        {/* <div key={index}>
+                                            <br />
+                                            <p> Item: {item.name}</p>
+                                            <p> Item Description: {item.description}</p>
+                                            <p> Item Price: {entries.finalPrice} ({entries.regularPrice - entries.finalPrice} off)</p>
+                                            <p>Item Rarity: {item.rarity.displayValue}</p>
+                                            <p>Item Image: {item.images.icon}</p>
+                                        </div> */}
+                                        
+                                        <div className="card">
+                                            <div className="card-image" style={{ backgroundImage: `url(${item.images.icon})`}}></div>
+                                            <h2 className="cardTextColor">{item.name}</h2>
+                                            <p className="cardTextColor">{item.description}</p>
+
+                                            {entries.regularPrice - entries.finalPrice > 0 &&
+                                                <a href="">{entries.finalPrice} ({entries.regularPrice - entries.finalPrice} off!)</a>
+                                            }
+                                            
+                                            {entries.regularPrice - entries.finalPrice <= 0&&
+                                                <a href="">{entries.finalPrice}</a>
+                                            }
+                                        </div>
+                                        
+                                    </>
+                                )}
+                            </>
+                        );
+                    }}
+                )}
             </section>
-            
-            {/* {shopEntries.map((entries, index) =>
-                
-                {if (entries.bundle !== null) {
-                    return (
-                        <>
-                            <div key={index}>
-                                <br />
-                                <p>Bundle {index}: {entries.bundle.name}</p>
-                                <p>Image: {entries.bundle.image} </p>
-                                <p> Price: {entries.finalPrice} ({entries.regularPrice - entries.finalPrice} off!)</p>
-                            </div>
-                            
-                        </>
-                    )
-                } else {
-                    return (
-                        <>
-                            {entries.items.map((item, index) =>
-                                <>
-                                    <div key={index}>
-                                        <br />
-                                        <p> Item: {item.name}</p>
-                                        <p> Item Description: {item.description}</p>
-                                        <p> Item Price: {entries.finalPrice} ({entries.regularPrice - entries.finalPrice} off)</p>
-                                        <p>Item Rarity: {item.rarity.displayValue}</p>
-                                        <p>Item Image: {item.images.icon}</p>
-                                    </div>
-                                </>
-                            )}
-                        </>
-                    );
-                }}
-                 
-            )} */}
         </div>
         </>
     )
