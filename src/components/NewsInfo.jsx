3def3b565c2e08bab07c01c213b0ade0942433bd
MochaWaffle <vsaini23@medford.k12.ma.us>
2024-01-01 13:12:34 -0500

    Refactored news code.

---
+++ src/components/NewsInfo.jsx
@@ -0,0 +1,31 @@
+export default function NewsInfo({newsTitle, newsText, newsImage, newsType}) {
+    return (
+        <>
+            <h1 className="newsTitle">{newsType} News</h1>
+                {newsTitle.length > 0 && newsTitle !== "Error" &&
+                    <>
+                        <h2> {newsTitle} </h2>
+                    
+                        <div className = "newsContainer">
+                            <div className="newsText">
+                                <p><i>{newsText}</i></p>
+                            </div>
+                            
+                            <a href={newsImage} target="_blank"><img src={newsImage} alt="Image could not load. Image was: Current Fortnite News Image."></img></a>
+                            <div className="newsText">
+                                <p>Click <a href={newsImage} target="_blank">image</a> to enlarge it.</p>
+                            </div>
+                        </div>
+                    </>
+                }
+                
+                {newsTitle == "Error" &&
+                    <h2> Error fetching {newsType} news data.</h2>
+                }
+
+                {newsTitle.length == 0 &&
+                    <h2> Currently no {newsType} news.</h2>
+                }
+        </>
+    )
+    }
