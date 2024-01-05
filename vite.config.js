04a1b234399dcee6de08652b0ffef762c474e5e3
MochaWaffle <vsaini23@medford.k12.ma.us>
2024-01-05 11:42:32 -0500

    Updated manifest
    Installed vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+        manifest: {
+          icons: [
+            {
+              src: "/icons/manifest-icon-144.png",
+              sizes: "144x144",
+              type: "image/png",
+              purpose: "any"
+            },
+            {
+              src: "/icons/manifest-icon-192.maskable.png",
+              sizes: "192x192",
+              type: "image/png",
+              purpose: "any"
+            },
+            {
+              src: "/icons/manifest-icon-192.maskable.png",
+              sizes: "192x192",
+              type: "image/png",
+              purpose: "maskable"
+            },
+            {
+              src: "/icons/manifest-icon-512.maskable.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any"
+            },
+            {
+              src: "/icons/manifest-icon-512.maskable.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable"
+            }
+          ]
+        }
+    })
+  ],
 })
