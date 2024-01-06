6f25b56a8e248a4a75468798542f630b72fca508
MochaWaffle <vsaini23@medford.k12.ma.us>
2024-01-06 10:31:15 -0500

    Added credits on home.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,14 @@ function Home() {
                 <p className="homeBodyText">This website uses the API: <a href="https://fortnite-api.com/">https://fortnite-api.com/</a></p>
             </div>
         </div>
+
+        <footer>
+            <div className="parentFooterText">
+                <div className="footerText">
+                    <p> Made by: <a href="https://github.com/MochaWaffle">MochaWaffle</a> on GitHub</p>
+                </div> 
+            </div>
+        </footer>
         </>
     )
 }
