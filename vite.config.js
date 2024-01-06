ecca5ce280d6d0b5d9eaa815c23c4a54b3ae3a2b
MochaWaffle <vsaini23@medford.k12.ma.us>
2024-01-06 12:51:56 -0500

    Updated config

--- vite.config.js
+++ vite.config.js
@@ -7,63 +7,65 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-        manifest: {
-          name: "FortniteInfo",
-          short_name: "FortniteInfo",
-          display: "standalone",
-          background_color: "#086BBE",
-          theme_color: "#086BBE",
-          orientation: "portrait",
-          description: "FortniteInfo shows Fortnite's current map, news, and shop items and checks the validity of a creator code.",
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "FortniteInfo",
+        short_name: "FortniteInfo",
+        display: "standalone",
+        background_color: "#086BBE",
+        theme_color: "#086BBE",
+        orientation: "portrait",
+        description: "FortniteInfo shows Fortnite's current map, news, and shop items and checks the validity of a creator code.",
 
-          icons: [
-            {
-              src: "/icons/manifest-icon-144.png",
-              sizes: "144x144",
-              type: "image/png",
-              purpose: "any"
-            },
-            {
-              src: "/icons/manifest-icon-192.maskable.png",
-              sizes: "192x192",
-              type: "image/png",
-              purpose: "any"
-            },
-            {
-              src: "/icons/manifest-icon-192.maskable.png",
-              sizes: "192x192",
-              type: "image/png",
-              purpose: "maskable"
-            },
-            {
-              src: "/icons/manifest-icon-512.maskable.png",
-              sizes: "512x512",
-              type: "image/png",
-              purpose: "any"
-            },
-            {
-              src: "/icons/manifest-icon-512.maskable.png",
-              sizes: "512x512",
-              type: "image/png",
-              purpose: "maskable"
+        icons: [
+          {
+            src: "icons/manifest-icon-144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: "icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*'],
+        runtimeCaching: [{
+          urlPattern: ({ url }) => {
+            console.log("Href: " + url.href)
+            return url.href.includes("fortnite-api.com");
+          },
+          handler: "CacheFirst",
+          options: {
+            cacheName: "api-cache",
+            cacheableResponse: {
+              statuses: [0, 200]
             }
-          ]
-        },
-        workbox: {
-          runtimeCaching: [{
-            urlPattern: ({ url }) => {
-              console.log("Href: " + url.href)
-              return url.href.includes("fortnite-api.com");
-            },
-            handler: "CacheFirst",
-            options: {
-              cacheName: "api-cache",
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          }]
-        }
+          }
+        }]
+      }
     })
   ],
 })
