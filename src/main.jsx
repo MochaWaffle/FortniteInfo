eebcfdc0b1c23cb19bbf3d60a49dc79198031f93
MochaWaffle <vsaini23@medford.k12.ma.us>
2024-01-11 11:54:41 -0500

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import "./App.css"
+import { registerSW} from 'virtual:pwa-register'
+
+registerSW({immediate: true})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
